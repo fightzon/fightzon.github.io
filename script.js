e55df4957acd006550345c6bf84a39ac6a1bf9f0
shiv <rathodshivdharmavirsingh@gmail.com>
2025-08-19 12:16:40 +0530

    Update script.js
    Signed-off-by: shiv <rathodshivdharmavirsingh@gmail.com>

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById("feedback-form").addEventListener("submit", function(e) 
   e.preventDefault();
   alert("Thanks for your message! I'll reach out soon.");
 });
+function login() {
+  window.location.href = "home.html";
+  return false;
+}

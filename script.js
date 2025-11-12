fb06387c0be9815099df326da37e8f555a2523b3
shiv <rathodshivdharmavirsingh@gmail.com>
2025-11-12 15:54:25 +0530

    Delete script.js
    Signed-off-by: shiv <rathodshivdharmavirsingh@gmail.com>

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("feedback-form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  alert("Thanks for your message! I'll reach out soon.");
-});
-function login() {
-  window.location.href = "home.html";
-  return false;
-}
